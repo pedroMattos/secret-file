c855cf548283d08fd2135e2dbb73b9815bfa425c
Pedro Mattos <47367479+pedroMattos@users.noreply.github.com>
2023-05-05 14:40:53 -0300

    fix validate function

--- src/composables/checkIsValidUser.js
+++ src/composables/checkIsValidUser.js
@@ -1,24 +1,25 @@
 import router from "@/router";
 import * as user from "@/models/services/localUserData";
-const VALID_TOKEN_TIMESTAMP_LIMIT = 1683300625735
+const VALID_TOKEN_TIMESTAMP_LIMIT = 3600000;
 function validToken(userData) {
-  const date = new Date()
-  const timestamp = date.getTime()
+  const date = new Date();
+  const timestamp = date.getTime();
+  if (!userData.at(0)?.lastLogin) return false;
   if (timestamp - userData.at(0)?.lastLogin >= VALID_TOKEN_TIMESTAMP_LIMIT) {
-    user.deleteUserRegisterById(userData.at(0)?.uuid)
-    return false
+    user.deleteUserRegisterById(userData.at(0)?.uuid);
+    return false;
   }
 
-  return true
+  return true;
 }
 
 export default async function checkIsValidUser() {
-  const userData = await user.getUserData()
-    if (!validToken(userData)) {
-      router.push("/");
+  const userData = await user.getUserData();
+  if (!validToken(userData)) {
+    router.push("/");
 
-      return;
-    }
+    return;
+  }
 
-    return true
+  return true;
 }
