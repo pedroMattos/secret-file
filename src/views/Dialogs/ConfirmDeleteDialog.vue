f662cbf2c8efd6d9df4a8b2761975cacaab2c6c8
Pedro Mattos <47367479+pedroMattos@users.noreply.github.com>
2023-05-06 23:24:45 -0300

    add confirm delete dialog

---
+++ src/views/Dialogs/ConfirmDeleteDialog.vue
@@ -0,0 +1,28 @@
+<script setup>
+import {ref} from 'vue'
+
+const dialog = ref(false)
+</script>
+
+<template>
+    <v-dialog
+      v-model="dialog"
+      width="auto"
+    >
+      <template v-slot:activator="{ props }">
+        <v-btn v-bind="props" @click="handleDelete" icon variant="text">
+          <s-icon icon-name="trash-alt" />
+        </v-btn>
+      </template>
+
+      <v-card>
+        <v-card-text>
+          Deseja apagar esse arquivo?
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary" @click="dialog = false">Apagar</v-btn>
+          <v-btn color="secondary" @click="dialog = false">Cancelar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+</template>
--- src/views/Home/FileActions/DeleteAction/DeleteAction.vue
+++ src/views/Home/FileActions/DeleteAction/DeleteAction.vue
@@ -1,24 +1,23 @@
 <script setup>
-import checkIsValidUser from "@/composables/checkIsValidUser";
-import * as file from "@/models/services/localFile";
-import { defineEmits, defineProps } from "vue";
-const emit = defineEmits(["delete"]);
-const props = defineProps({
-  itemId: Number,
-});
+// import checkIsValidUser from "@/composables/checkIsValidUser";
+// import * as file from "@/models/services/localFile";
+import ConfirmDeleteDialog from "@/views/Dialogs/ConfirmDeleteDialog.vue";
+// import { defineEmits, defineProps } from "vue";
+// const emit = defineEmits(["delete"]);
+// const props = defineProps({
+//   itemId: Number,
+// });
 
 async function handleDelete() {
-  const isValid = await checkIsValidUser();
-  if (!isValid) return;
-  file.deleteById(props.itemId);
-  emit("delete");
+  // const isValid = await checkIsValidUser();
+  // if (!isValid) return;
+  // file.deleteById(props.itemId);
+  // emit("delete");
 }
 </script>
 <template>
   <div class="lock">
-    <v-btn @click="handleDelete" icon variant="text">
-      <s-icon icon-name="trash-alt" />
-    </v-btn>
+    <confirm-delete-dialog @onConfirm="handleDelete" />
   </div>
 </template>
 
