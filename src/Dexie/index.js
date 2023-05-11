fcb36c8bad5a0232f98a50d715ad927b0f6eda61
Pedro Mattos <47367479+pedroMattos@users.noreply.github.com>
2023-05-11 13:02:29 -0300

    fix localfiles with uuid and update database

--- src/Dexie/index.js
+++ src/Dexie/index.js
@@ -2,8 +2,8 @@ import Dexie from "dexie";
 
 export const db = new Dexie("myFiles");
 export const dbUser = new Dexie("userData");
-db.version(1).stores({
-  files: "++id, name, content, password, file, uuid",
+db.version(2).stores({
+  files: "++id, name, content, uuid, password, file",
 });
 dbUser.version(1).stores({
   user: "uuid, name, lastLogin",
--- src/models/services/localFile.js
+++ src/models/services/localFile.js
@@ -1,13 +1,9 @@
 import { db } from "@/Dexie";
 import * as user from "./localUserData";
 
-export function getData() {
-  try {
-    const data = db.files.reverse().toArray();
-    return data;
-  } catch (error) {
-    return error;
-  }
+export async function getData() {
+  const userData = await user.getUserData();
+  return db.files.where("uuid").equals(userData.at(0).uuid).reverse().toArray();
 }
 
 export async function makeLocalFileUniqueByUser() {
