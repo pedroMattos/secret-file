e5b6c9865a8c8cb8a2efc86eb4e05fbf1e584e62
Pedro Mattos <47367479+pedroMattos@users.noreply.github.com>
2023-05-16 09:57:48 -0300

    update db version

--- src/Dexie/index.js
+++ src/Dexie/index.js
@@ -2,8 +2,9 @@ import Dexie from "dexie";
 
 export const db = new Dexie("myFiles");
 export const dbUser = new Dexie("userData");
-db.version(5).stores({
-  files: "++id, name, content, uuid, password, file, inCloud, fileType, cloudId",
+db.version(6).stores({
+  files:
+    "++id, name, content, uuid, password, file, inCloud, fileType, cloudId",
 });
 dbUser.version(2).stores({
   user: "uuid, name, lastLogin, userName",
