41d46f1b2ee2fb9ac3956ff27e13c709ceec8bf8
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-05-23 19:37:17 -0300

    change icon color

--- src/views/Dialogs/ConfirmDeleteDialog.vue
+++ src/views/Dialogs/ConfirmDeleteDialog.vue
@@ -1,34 +1,29 @@
 <script setup>
-import {ref, defineEmits} from 'vue'
+import { ref, defineEmits } from "vue";
 
-const emits = defineEmits(['confirm-delete'])
+const emits = defineEmits(["confirm-delete"]);
 
-const dialog = ref(false)
+const dialog = ref(false);
 function handleDelete() {
-  dialog.value = false
-  emits('confirm-delete')
+  dialog.value = false;
+  emits("confirm-delete");
 }
 </script>
 
 <template>
-    <v-dialog
-      v-model="dialog"
-      width="auto"
-    >
-      <template v-slot:activator="{ props }">
-        <v-btn v-bind="props" icon variant="text">
-          <s-icon icon-name="trash-alt" />
-        </v-btn>
-      </template>
+  <v-dialog v-model="dialog" width="auto">
+    <template v-slot:activator="{ props }">
+      <v-btn v-bind="props" icon variant="text">
+        <s-icon icon-name="trash-alt" icon-color="#E57373" />
+      </v-btn>
+    </template>
 
-      <v-card>
-        <v-card-text>
-          Deseja apagar esse arquivo?
-        </v-card-text>
-        <v-card-actions>
-          <v-btn color="secondary" @click="dialog = false">Cancelar</v-btn>
-          <v-btn color="primary" @click="handleDelete">Apagar</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
+    <v-card>
+      <v-card-text> Deseja apagar esse arquivo? </v-card-text>
+      <v-card-actions>
+        <v-btn color="secondary" @click="dialog = false">Cancelar</v-btn>
+        <v-btn color="primary" @click="handleDelete">Apagar</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
