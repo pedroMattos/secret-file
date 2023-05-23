e83991c7eb89a0a212cc0c31638584c7583ec43c
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-05-23 19:51:45 -0300

    add store options

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,15 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    refetchCloudItems: false
   },
   getters: {
+    refetchCloudItems: (state) => state.refetchCloudItems
   },
   mutations: {
+    refetchCloudItems: (state, payload) => {
+      state.refetchCloudItems = payload
+    }
   },
   actions: {
   },
