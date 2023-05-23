6313d1b9d6a43d1a6a291a534d8599197195364b
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-05-23 19:51:33 -0300

    add computed store to refetch items

--- src/composables/useCloudBackup.js
+++ src/composables/useCloudBackup.js
@@ -1,9 +1,11 @@
-import { ref } from 'vue'
+import { ref, computed, watch } from 'vue'
+import { useStore } from "vuex";
 import * as cloud from '@/models/services/cloud'
 import * as local from '@/models/services/localFile'
 import * as user from '@/models/services/localUserData'
 
 export function useCloudBakcup() {
+  const store = useStore()
   const filesInCloud = ref([])
   const filesInLocal = ref(null)
   const userId = ref(null)
@@ -17,6 +19,25 @@ export function useCloudBakcup() {
 
   local.getData().then((data) => {
     filesInLocal.value = data
+  })
+
+  const startRefetch = computed(() => store.getters.refetchCloudItems)
+
+  watch(startRefetch, (value) => {
+    if (value) {
+      user.getUserData().then((userData) => {
+        userId.value = userData.at(0).uuid
+        cloud.getFiles(userId.value).then((cloudFiles) => {
+          filesInCloud.value = cloudFiles
+        })
+      })
+    
+      local.getData().then((data) => {
+        filesInLocal.value = data
+      })
+
+      store.commit("refetchCloudItems", false)
+    }
   })
 
   return { filesInCloud, filesInLocal }
