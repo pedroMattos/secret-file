fe1c89961d999717474ff29caf6fd276b6001bd1
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-05-23 19:52:10 -0300

    add store commits

--- src/views/Home/FileActions/DeleteAction/DeleteAction.vue
+++ src/views/Home/FileActions/DeleteAction/DeleteAction.vue
@@ -1,8 +1,11 @@
 <script setup>
 import checkIsValidUser from "@/composables/checkIsValidUser";
 import * as file from "@/models/services/localFile";
+import { useStore } from "vuex";
 import ConfirmDeleteDialog from "@/views/Dialogs/ConfirmDeleteDialog.vue";
 import { defineEmits, defineProps } from "vue";
+
+const store = useStore();
 const emit = defineEmits(["delete"]);
 const props = defineProps({
   itemId: Number,
@@ -12,6 +15,7 @@ async function handleDelete() {
   const isValid = await checkIsValidUser();
   if (!isValid) return;
   file.deleteById(props.itemId);
+  store.commit("refetchCloudItems", true);
   emit("delete");
 }
 </script>
