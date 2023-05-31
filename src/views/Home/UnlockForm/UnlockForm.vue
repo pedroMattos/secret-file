3aac33738b844baa1e348d1a7949495d2c54e698
Pedro Mattos <laranjeiraspedro@gmail.com>
2023-05-30 22:51:21 -0300

    decrypt password to unlock

--- src/views/Home/UnlockForm/UnlockForm.vue
+++ src/views/Home/UnlockForm/UnlockForm.vue
@@ -1,26 +1,25 @@
 <script setup>
-import {ref, defineEmits, defineProps} from 'vue'
-const emit = defineEmits(['cancel', 'unlockFile'])
+import decryptData from "@/composables/useDecrypt";
+import { ref, defineEmits, defineProps } from "vue";
+const emit = defineEmits(["cancel", "unlockFile"]);
 const props = defineProps({
-  pass: String
-})
-const filePass = ref(null)
-const message = ref(null)
+  pass: String,
+});
+const filePass = ref(null);
+const message = ref(null);
 
 function handleUnlock() {
-  if (props.pass === filePass.value) {
-    emit('unlockFile')
-    return
+  if (decryptData(props.pass) === filePass.value) {
+    emit("unlockFile");
+    return;
   }
 
-  message.value = 'Senha incorreta! Tente novamente'
+  message.value = "Senha incorreta! Tente novamente";
 }
 </script>
 <template>
   <div class="unlock">
-    <s-text>
-      Arquivo bloqueado por senha
-    </s-text>
+    <s-text> Arquivo bloqueado por senha </s-text>
     <v-text-field
       v-model="filePass"
       autocomplete="new-password"
@@ -35,7 +34,9 @@ function handleUnlock() {
     </s-text>
     <div class="actions">
       <v-btn class="bg-red-darken-1" @click="emit('cancel')"> Cancelar </v-btn>
-      <v-btn class="bg-blue-lighten-1" @click="handleUnlock"> Desbloquear </v-btn>
+      <v-btn class="bg-blue-lighten-1" @click="handleUnlock">
+        Desbloquear
+      </v-btn>
     </div>
   </div>
 </template>
